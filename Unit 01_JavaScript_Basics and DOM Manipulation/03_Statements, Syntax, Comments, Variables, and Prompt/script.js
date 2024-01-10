8eb2f2e70b33508d8b5084ace544f7a2997dd7ea
Riley Dean <deanril00@warren.k12.in.us>
2024-01-10 12:15:24 -0500

    updates

---
+++ Unit 01_JavaScript_Basics and DOM Manipulation/03_Statements, Syntax, Comments, Variables, and Prompt/script.js
@@ -0,0 +1,7 @@
+
+let Firstname = prompt("What is your first name?");
+let Lastname = prompt("What is your last name?");
+
+let Fullname = Firstname +" " + Lastname;
+
+document.getElementById("name").innerHTML = Fullname;
