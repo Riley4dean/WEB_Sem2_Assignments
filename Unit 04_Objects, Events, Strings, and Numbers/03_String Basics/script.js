91e39af94ab7084ae26e958c762969ce9dcfd108
Riley Dean <deanril00@warren.k12.in.us>
2024-02-02 11:46:51 -0500

    let rick roll

---
+++ Unit 04_Objects, Events, Strings, and Numbers/03_String Basics/script.js
@@ -0,0 +1,12 @@
+let text = 'Never gonna give you up'
+document.getElementById("demo").innerHTML = text;
+let text2 = 'Never gonna let you down'
+document.getElementById("demo2").innerHTML = text2;
+let text3 = 'Never gonna run around and desert you'
+document.getElementById("demo3").innerHTML = text3;
+let text4 = 'Never gonna make you cry'
+document.getElementById("demo4").innerHTML = text4;
+let text5 = 'Never gonna say goodbye'
+document.getElementById("demo5").innerHTML = text5;
+let text6 = 'Never gonna tell a lie and hurt you'
+document.getElementById("demo6").innerHTML = text6;
