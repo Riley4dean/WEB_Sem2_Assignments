250a3b838b56c5257997c17d01afb0ad2b29be75
Riley Dean <deanril00@warren.k12.in.us>
2024-02-07 11:50:38 -0500

    maths

--- Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
@@ -14,3 +14,21 @@ function takeAbsoluteValue(){
     let absoluteValue = Math.abs(number);
     document.getElementById("result2").innerHTML = absoluteValue;
 }
+
+function takeCos(){
+    let number = +document.getElementById("input3").value;
+    let Cos = Math.cos(number);
+    document.getElementById("result3").innerHTML = Cos;
+}
+function takeSin(){
+    let number = +document.getElementById("input4").value;
+    let Sin = Math.sign(number);
+    document.getElementById("result4").innerHTML = Sin;
+}
+function takeTan(){
+    let number = +document.getElementById("input5").value;
+    let tan = Math.tan(number);
+    document.getElementById("result5").innerHTML = tan;
+}
+
+
